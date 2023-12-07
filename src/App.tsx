e8aa8f2b11b3434c010508d014f7b979442070e4
Alexei <avenge.now.r@mail.ru>
2023-12-07 22:12:02 +0300

    feat[task-3]: add public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Welcome from './pages/Welcome';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Welcome />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/registration',
+    element: <Registration />,
+  },
+]);
 
 function App() {
-  return;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Registration/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import { Wrapper } from '../../components/Wrapper';
+
+const Registration = () => {
+  return (
+    <Wrapper>
+      <Typography variant="h1">Registration</Typography>
+    </Wrapper>
+  );
+};
+
+export default Registration;
