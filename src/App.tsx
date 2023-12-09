4aff5532c01d5a36bddfc6ea9d889d1b17549ed7
Alexei <avenge.now.r@mail.ru>
2023-12-09 09:35:49 +0300

    refactor[task-9]: remove unusual app.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
 import Welcome from './pages/Welcome';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
