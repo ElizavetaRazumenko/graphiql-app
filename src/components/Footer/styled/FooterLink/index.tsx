fbc6b8f0a90b9132e3ad18493f84bce19ca4e6fd
Alexei <avenge.now.r@mail.ru>
2023-12-09 19:07:40 +0300

    feat[task-9]: add hover to links, add responsive font sizing

--- src/components/Footer/styled/FooterLink/index.tsx
+++ src/components/Footer/styled/FooterLink/index.tsx
@@ -1,10 +1,19 @@
 import MaterialLink, {
   LinkProps as MaterialLinkProps,
 } from '@mui/material/Link';
-import { styled } from '@mui/material/styles';
+import { Theme, styled } from '@mui/material/styles';
 
-export const FooterLink = styled(MaterialLink)<MaterialLinkProps>(() => ({
-  textDecoration: 'none',
-  color: '#000',
-  fontSize: '16px',
-}));
+export const FooterLink = styled(MaterialLink)<MaterialLinkProps>(
+  ({ theme }: { theme: Theme }) => ({
+    textDecoration: 'none',
+    color: '#000',
+    fontSize: '24px',
+    transition: '.2s ease',
+    ':hover': {
+      textShadow: '0px 0px 2px #000',
+    },
+    [theme.breakpoints.down('md')]: {
+      fontSize: '16px',
+    },
+  }),
+);
