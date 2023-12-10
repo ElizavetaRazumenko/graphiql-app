2950f6d797408df8400328de288edc20d1bce683
vpyadus <41780056+vpyadus@users.noreply.github.com>
2023-12-10 22:32:49 +0300

    feat[task-10]: add header into LayoutWithErrorHandling

--- src/components/LayoutWithErrorHandling/index.tsx
+++ src/components/LayoutWithErrorHandling/index.tsx
@@ -2,10 +2,12 @@ import { Outlet } from 'react-router-dom';
 import ErrorBoundary from '../ErrorBoundary';
 import { Footer } from '../Footer';
 import { MainWrapper } from './styled';
+import { Header } from '../Header';
 
 const LayoutWithErrorHandling = () => {
   return (
     <ErrorBoundary>
+      <Header />
       <MainWrapper>
         <Outlet />
       </MainWrapper>
