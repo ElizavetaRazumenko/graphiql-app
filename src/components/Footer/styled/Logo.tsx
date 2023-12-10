67237c09c63c42989b43988f1c857e4dd2f260ba
Alexei <avenge.now.r@mail.ru>
2023-12-10 22:21:41 +0300

    refactor[task-9]: change initial image width

--- src/components/Footer/styled/Logo.tsx
+++ src/components/Footer/styled/Logo.tsx
@@ -1,6 +1,7 @@
 import { Theme, styled } from '@mui/material/styles';
 
 export const Logo = styled('img')(({ theme }: { theme: Theme }) => ({
+  width: '115px',
   [theme.breakpoints.down('sm')]: {
     width: '85px',
   },
