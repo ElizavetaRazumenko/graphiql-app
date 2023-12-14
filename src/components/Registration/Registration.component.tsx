23c2fb3fdd16043b7db547fcb4ebe2736bc00ccb
vpyadus <41780056+vpyadus@users.noreply.github.com>
2023-12-15 02:35:50 +0300

    feat[task-22]: add register form handling, validation and submitting to Firebase

--- src/components/Registration/Registration.component.tsx
+++ src/components/Registration/Registration.component.tsx
@@ -4,6 +4,17 @@ import { Input } from '../../shared/Input';
 import { Button } from '../../shared/Button';
 import { useContext } from 'react';
 import { localizationContext } from '../../context/localizationContext';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { registerWithEmailAndPassword } from '../../firebase';
+import registrationFormSchema from '../../validationSchemas/registrationFormSchema';
+
+export type RegisterFormFields = {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+};
 
 const Registration = () => {
   const {
@@ -19,26 +30,70 @@ const Registration = () => {
     },
   } = useContext(localizationContext);
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<RegisterFormFields>({
+    mode: 'onChange',
+    resolver: yupResolver(registrationFormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+  });
+
+  const onSubmitHandler: SubmitHandler<RegisterFormFields> = (
+    data: RegisterFormFields,
+  ): void => {
+    console.log({ data });
+    registerWithEmailAndPassword(data.name, data.email, data.password);
+    reset();
+  };
+
   return (
     <AuthDialog title={title}>
-      <Box component="form">
-        <Stack gap={4}>
-          <Input placeholder={name} variant="outlined" icon="user" />
-          <Input placeholder={email} variant="outlined" icon="email" />
+      <Box component="form" onSubmit={handleSubmit(onSubmitHandler)}>
+        <Stack gap={3}>
+          <Input
+            placeholder={name}
+            variant="outlined"
+            icon="user"
+            {...register('name')}
+            error={Boolean(errors.name)}
+            helperText={errors.name?.message ?? '\u00A0'}
+          />
+          <Input
+            placeholder={email}
+            variant="outlined"
+            icon="email"
+            {...register('email')}
+            error={Boolean(errors.email)}
+            helperText={errors.email?.message ?? '\u00A0'}
+          />
           <Input
             placeholder={password}
             variant="outlined"
             type="password"
             icon="pass"
+            {...register('password')}
+            error={Boolean(errors.password)}
+            helperText={errors.password?.message ?? '\u00A0'}
           />
           <Input
             placeholder={passwordRepeat}
             variant="outlined"
             type="password"
             icon="pass"
+            {...register('passwordConfirm')}
+            error={Boolean(errors.passwordConfirm)}
+            helperText={errors.passwordConfirm?.message ?? '\u00A0'}
           />
         </Stack>
-        <Button type="submit" variant="contained">
+        <Button type="submit" variant="contained" disabled={!isValid}>
           {submit}
         </Button>
       </Box>
--- src/shared/Input/Input.component.tsx
+++ src/shared/Input/Input.component.tsx
@@ -4,6 +4,7 @@ import { styled, Theme } from '@mui/material/styles';
 import PersonIcon from '@mui/icons-material/Person';
 import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
+import { ForwardedRef, forwardRef } from 'react';
 
 export type InputIconType = 'email' | 'pass' | 'user';
 
@@ -17,40 +18,49 @@ const iconToSVGMap: Record<InputIconType, JSX.Element> = {
   user: <PersonIcon color="primary" fontSize="large" />,
 };
 
-const TextFieldWithAdornment = (props: InputProps) => {
-  const { icon } = props;
-  const svgIcon: JSX.Element = iconToSVGMap[icon];
-  return (
-    <TextField
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">{svgIcon}</InputAdornment>
-        ),
-      }}
-      {...props}
-    />
-  );
-};
+const TextFieldWithAdornment = forwardRef(
+  (props: InputProps, ref: ForwardedRef<HTMLInputElement>) => {
+    const { icon } = props;
+    const svgIcon: JSX.Element = iconToSVGMap[icon];
+    return (
+      <TextField
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">{svgIcon}</InputAdornment>
+          ),
+        }}
+        {...props}
+        ref={ref}
+      />
+    );
+  },
+);
 
 const Input = styled(TextFieldWithAdornment)<InputProps>(
   ({ theme }: { theme: Theme }) => ({
-    backgroundColor: 'rgba(24, 10, 46, 0.42)',
-    boxShadow: '3px 3px 10px 8px rgba(0, 0, 0, 0.10) inset',
-    [theme.breakpoints.down('sm')]: {
-      borderRadius: '5px',
-    },
-    [theme.breakpoints.up('sm')]: {
-      borderRadius: '15px',
+    '& .MuiInputBase-root': {
+      color: theme.palette.primary.main,
+      border: '1px solid transparent',
+      backgroundColor: 'rgba(24, 10, 46, 0.42)',
+      boxShadow: '3px 3px 10px 8px rgba(0, 0, 0, 0.10) inset',
+      [theme.breakpoints.down('sm')]: {
+        borderRadius: '5px',
+      },
+      [theme.breakpoints.up('sm')]: {
+        borderRadius: '15px',
+      },
     },
     '& fieldset': {
       border: 'none',
     },
-    '& .MuiInputBase-root': {
-      color: theme.palette.primary.main,
-    },
     '& .MuiInputAdornment-root': {
       justifyContent: 'center',
       width: '48px',
+    },
+    '& .MuiInputBase-root.Mui-error': {
+      border: '1px solid red',
+      boxShadow:
+        '3px 3px 10px 8px rgba(0, 0, 0, 0.10) inset, 0px 0px 20px 15px rgba(255, 16, 16, 0.15)',
     },
   }),
 );
