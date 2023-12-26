789ec7a9729dfe2b1a92286c4e7f044407718c64
vpyadus <41780056+vpyadus@users.noreply.github.com>
2023-12-27 00:26:13 +0300

    feat[task-51]: add documentation

--- src/components/DocumentationModal/DocumentationModal.component.tsx
+++ src/components/DocumentationModal/DocumentationModal.component.tsx
@@ -1,30 +1,94 @@
+import { useEffect } from 'react';
+import { getQraphQLData } from '../../services/graphql';
+import { Spinner } from '../../shared/Spinner';
 import { CloseModalButton, ModalContainer, ModalList } from './styled';
+import { Box, Typography } from '@mui/material';
+import { AccordionItem } from '../../shared/AccordionItem';
+import getGraphQLDocumentationSchema, {
+  schemaParts,
+} from '../../utils/getGraphQLDocumentationSchema';
 
 type DocumentationModalProps = {
   isModalOpen: boolean;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
+const getDocumentationTree = (docItem: unknown): JSX.Element => {
+  if (docItem === null || docItem === undefined) return <>N/A</>;
+
+  if (docItem instanceof Array) {
+    return (
+      <>
+        {docItem.length === 0 && <Typography>-</Typography>}
+        {docItem.map((item: Record<string, unknown>, index: number) => (
+          <AccordionItem key={index} summary={item?.name as string}>
+            {getDocumentationTree(item)}
+          </AccordionItem>
+        ))}
+      </>
+    );
+  }
+
+  if (docItem instanceof Object)
+    return (
+      <>
+        {Object.keys(docItem).map((key: string, index: number) => {
+          const value: unknown = (docItem as Record<string, unknown>)[key];
+          if (value === null) return null;
+          if (value instanceof Object) {
+            return (
+              <AccordionItem key={index} summary={key}>
+                {getDocumentationTree(value)}
+              </AccordionItem>
+            );
+          }
+          return (
+            <Box key={index}>
+              <Typography key={index}>{key}:</Typography>
+              {getDocumentationTree(value)}
+            </Box>
+          );
+        })}
+      </>
+    );
+
+  return <Typography>{(docItem as string) || '-'}</Typography>;
+};
+
 const DocumentationModal = ({
   isModalOpen,
   setIsModalOpen,
 }: DocumentationModalProps) => {
+  const [trigger, result] = getQraphQLData.useLazyGetSchemaQuery();
+  const { data, isLoading } = result;
+  const schema: Record<string, unknown> = data?.data.__schema;
+
+  useEffect(() => {
+    if (isModalOpen) trigger(getGraphQLDocumentationSchema());
+  }, [isModalOpen]);
+
   return (
-    <ModalContainer opened={isModalOpen.toString()}>
-      <CloseModalButton
-        onClick={() => setIsModalOpen(!isModalOpen)}
-      ></CloseModalButton>
-      <ModalList>
-        A GraphQL schema provides a root type for each kind of operation. Root
-        Types query: Root All Schema Types String Int FilmsConnection PageInfo
-        Boolean FilmsEdge Film Node ID FilmSpeciesConnection FilmSpeciesEdge
-        Species Float Planet PlanetResidentsConnection PlanetResidentsEdge
-        Person PersonFilmsConnection PersonFilmsEdge PersonStarshipsConnection
-        PersonStarshipsEdge Starship StarshipPilotsConnection StarshipPilotsEdge
-        StarshipFilmsConnection StarshipFilmsEdge PersonVehiclesConnection
-        PersonVehiclesEdge Vehicle
-      </ModalList>
-    </ModalContainer>
+    <>
+      {!isLoading && (
+        <ModalContainer opened={isModalOpen.toString()}>
+          <CloseModalButton
+            onClick={() => setIsModalOpen(!isModalOpen)}
+          ></CloseModalButton>
+          <ModalList>
+            <Typography>
+              A GraphQL schema provides a root type for each kind of operation.
+            </Typography>
+            {!!data &&
+              schemaParts.map((part: Record<string, string>, index: number) => (
+                <AccordionItem key={index} summary={part.title}>
+                  {getDocumentationTree(schema[part.name])}
+                </AccordionItem>
+              ))}
+          </ModalList>
+        </ModalContainer>
+      )}
+      <Spinner open={isLoading} />
+    </>
   );
 };
 
--- src/components/DocumentationModal/styled/ModalContainer.tsx
+++ src/components/DocumentationModal/styled/ModalContainer.tsx
@@ -6,10 +6,10 @@ export const ModalContainer = styled(MaterialBox)<
 >(({ theme, opened }: { theme: Theme; opened: string }) => ({
   borderRadius: '30px',
   background: theme.palette.primary.main,
-  padding: '100px 25px 80px 65px',
+  padding: '60px 15px 30px 30px',
   boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-
-  maxWidth: '450px',
+  width: '50vw',
+  maxWidth: '550px',
   height: '90vh',
   zIndex: 100,
   position: 'fixed',
@@ -23,5 +23,7 @@ export const ModalContainer = styled(MaterialBox)<
     display: opened === 'true' ? 'block' : 'none',
     position: 'static',
     transform: `translate(0, 0)`,
+    width: '85vw',
+    maxWidth: '85vw',
   },
 }));
