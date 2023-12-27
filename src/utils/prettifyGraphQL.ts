953e9f141d885a2f9150a04a35fcd275d38da32e
Alexei <avenge.now.r@mail.ru>
2023-12-27 23:32:07 +0300

    feat: add advanced prettify

--- src/utils/prettifyGraphQL.ts
+++ src/utils/prettifyGraphQL.ts
@@ -1,23 +1,24 @@
-export default function prettifyGraphQL(query: string) {
-  const lines = query.split('\n');
-  const indents: number[] = [];
+export default function prettifyGraphQL(str: string) {
+  let result = '';
+  let indentation = 0;
 
-  lines.forEach((line, index) => {
-    const openingBraces = (line.match(/{/g) || []).length;
-    const closingBraces = (line.match(/}/g) || []).length;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '{') {
+      result += '{\n';
+      indentation += 2;
+      result += ' '.repeat(indentation);
+    } else if (str[i] === '}') {
+      result += '\n';
+      indentation -= 2;
+      result += ' '.repeat(indentation);
+      result += '}';
+    } else if (str[i] === ',') {
+      result += ',\n';
+      result += ' '.repeat(indentation);
+    } else {
+      result += str[i];
+    }
+  }
 
-    const previousIndent = index > 0 ? indents[index - 1] : 0;
-
-    const currentIndent = previousIndent + openingBraces - closingBraces;
-    indents.push(currentIndent);
-  });
-
-  const formattedQuery = lines
-    .map((line, index) => {
-      const indent = ' '.repeat(indents[index] * 2);
-      return (index === 0 ? '' : indent) + line.trim();
-    })
-    .join('\n');
-
-  return formattedQuery;
+  return result;
 }
