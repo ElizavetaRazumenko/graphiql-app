cd2d9210cb2bba77cd4e2529e28ee0a49e54b552
Alexei <avenge.now.r@mail.ru>
2023-12-29 09:25:52 +0300

    refactor: change welcome link styles

--- src/pages/NotFound/styled/WelcomeLink.tsx
+++ src/pages/NotFound/styled/WelcomeLink.tsx
@@ -2,8 +2,12 @@ import { Theme, styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 export const WelcomeLink = styled(Link)(({ theme }: { theme: Theme }) => ({
   color: theme.palette.primary.main,
+  textAlign: 'center',
   fontSize: '1.4rem',
   '&:hover': {
     color: theme.palette.primary.contrastText,
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '1rem',
   },
 }));
