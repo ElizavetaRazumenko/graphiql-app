d63a55fe9024b9bdc1d5199b588875ba78e02b9c
vpyadus <41780056+vpyadus@users.noreply.github.com>
2024-01-03 16:37:53 +0300

    feat[task-87]: add tests for Auth page, Login, Register, and QueryTabs components

--- src/components/Login/index.test.tsx
+++ src/components/Login/index.test.tsx
@@ -1,9 +1,16 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import { renderWithProviders } from '../../test';
 import Login from './Login.component';
 import { screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import {
+  MemoryRouter,
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from 'react-router-dom';
+import * as firebaseHooksAuth from 'react-firebase-hooks/auth';
+import { UserCredential } from 'firebase/auth';
 
 describe('Login', () => {
   it('should render Login', async () => {
@@ -18,5 +25,38 @@ describe('Login', () => {
     const title = await screen.findByText('Login');
 
     expect(title).toBeInTheDocument();
+  });
+
+  it('should redirect a user to Main if logged in', async () => {
+    const testRoutes: Array<RouteObject> = [
+      {
+        path: '/',
+        element: <Login />,
+      },
+      {
+        path: '/main',
+        element: <></>,
+      },
+    ];
+    const testRouter = createMemoryRouter(testRoutes, {
+      initialEntries: [`/`],
+    });
+
+    vi.spyOn(
+      firebaseHooksAuth,
+      'useSignInWithEmailAndPassword',
+    ).mockImplementation(
+      () =>
+        [
+          vi.fn(),
+          {} as UserCredential,
+          false,
+          undefined,
+        ] as firebaseHooksAuth.EmailAndPasswordActionHook,
+    );
+
+    renderWithProviders(<RouterProvider router={testRouter} />);
+
+    expect(testRouter.state.location.pathname).toBe('/main');
   });
 });
--- src/components/QueryTabs/index.test.tsx
+++ src/components/QueryTabs/index.test.tsx
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 
 import { renderWithProviders } from '../../test';
 import QueryTabs from './QueryTabs.component';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
 describe('QueryTabs', () => {
   it('should render QueryTabs', async () => {
@@ -17,5 +17,27 @@ describe('QueryTabs', () => {
 
     expect(variablesTab).toBeInTheDocument();
     expect(headersTab).toBeInTheDocument();
+  });
+
+  it('should switch between tabs on tab title click', async () => {
+    renderWithProviders(
+      <QueryTabs>
+        <span>Tab1</span>
+        <span>Tab2</span>
+      </QueryTabs>,
+    );
+
+    const firstTab = await screen.findByText('Tab1');
+    expect(firstTab).toBeInTheDocument();
+
+    const headersTab = await screen.findByText('Headers');
+    expect(headersTab).toBeInTheDocument();
+
+    fireEvent.click(headersTab);
+
+    expect(firstTab).not.toBeInTheDocument();
+
+    const secondTab = await screen.findByText('Tab2');
+    expect(secondTab).toBeInTheDocument();
   });
 });
