4a04cc58720e3c0bccf8aa3a21674853760632ab
Alexei <avenge.now.r@mail.ru>
2024-01-03 21:01:35 +0300

    feat[task-36]: add suspense feature

--- src/components/LayoutWithErrorHandling/index.tsx
+++ src/components/LayoutWithErrorHandling/index.tsx
@@ -9,22 +9,26 @@ import { auth } from '../../firebase';
 import { OutletContext } from '../../shared/types/types';
 import { Provider } from 'react-redux';
 import { store } from '../../store';
+import { Suspense } from 'react';
+import { Spinner } from '../../shared/Spinner';
 
 const LayoutWithErrorHandling = () => {
   const [user, loading] = useIdToken(auth);
   const isAuthenticated: boolean = Boolean(user);
   return (
-    <LocalizationContext>
-      <ErrorBoundary>
-        <Provider store={store}>
-          <Header isAuthenticated={isAuthenticated} loading={loading} />
-          <MainWrapper>
-            <Outlet context={{ isAuthenticated, loading } as OutletContext} />
-          </MainWrapper>
-          <Footer />
-        </Provider>
-      </ErrorBoundary>
-    </LocalizationContext>
+    <Suspense fallback={<Spinner open />}>
+      <LocalizationContext>
+        <ErrorBoundary>
+          <Provider store={store}>
+            <Header isAuthenticated={isAuthenticated} loading={loading} />
+            <MainWrapper>
+              <Outlet context={{ isAuthenticated, loading } as OutletContext} />
+            </MainWrapper>
+            <Footer />
+          </Provider>
+        </ErrorBoundary>
+      </LocalizationContext>
+    </Suspense>
   );
 };
 
